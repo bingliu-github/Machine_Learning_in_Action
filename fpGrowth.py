645a48c16d5e7b405d8526d4ff94169bbef6ac4e
qiyuangong <qiyuangong@gmail.com>
2015-06-17 16:24:20 +0800

    Finish FP-Growth algorithm.

--- fpGrowth.py
+++ fpGrowth.py
@@ -52,9 +52,9 @@ def updateTree(items, inTree, headerTable, count):
         else:
             updateHeader(headerTable[items[0]][1],
                          inTree.children[items[0]])
-            if len(items) > 1:
-                updateTree(items[1::], inTree.children[items[0]],
-                           headerTable, count)
+    if len(items) > 1:
+        updateTree(items[1::], inTree.children[items[0]],
+                   headerTable, count)
 
 
 def updateHeader(nodeToTest, targetNode):
@@ -78,3 +78,35 @@ def createInitSet(dataSet):
     for trans in dataSet:
         retDict[frozenset(trans)] = 1
     return retDict
+
+
+def ascendTree(leafNode, prefixPath):
+    if leafNode.parent is not None:
+        prefixPath.append(leafNode.name)
+        ascendTree(leafNode.parent, prefixPath)
+
+
+def findPrefixPath(basePat, treeNode):
+    condPats = {}
+    while treeNode is not None:
+        prefixPath = []
+        ascendTree(treeNode, prefixPath)
+        if len(prefixPath) > 1:
+            condPats[frozenset(prefixPath[1:])] = treeNode.count
+        treeNode = treeNode.nodeLink
+    return condPats
+
+
+def mineTree(inTree, headerTable, minSup, preFix, freqItemList):
+    bigL = [v[0] for v in sorted(headerTable.items(),
+            key=lambda p: p[1])]
+    for basePat in bigL:
+        newFreqSet = preFix.copy()
+        newFreqSet.add(basePat)
+        freqItemList.append(newFreqSet)
+        condPattBases = findPrefixPath(basePat, headerTable[basePat][1])
+        myCondTree, myHead = createTree(condPattBases, minSup)
+        if myHead is not None:
+            print 'conditional tree for: ', newFreqSet
+            myCondTree.disp(1)
+            mineTree(myCondTree, myHead, minSup, newFreqSet, freqItemList)
